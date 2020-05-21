df446e0021a4f554a6f956e004a8d66a464314a8
Pratyush Harsh <pratyushharsh2015@gmail.com>
2020-05-21 20:45:39 +0530

    updated model

--- lib/person/model/address.dart
+++ lib/person/model/address.dart
@@ -1,7 +1,8 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
 
 @immutable
-class Address {
+class Address  extends Equatable{
   final String houseNo;
   final String street;
   final String city;
@@ -11,6 +12,39 @@ class Address {
   final String pincode;
   final String state;
 
-  Address(this.houseNo, this.street, this.city, this.ward, this.municipal,
-      this.district, this.pincode, this.state);
+  Address({this.houseNo, this.street, this.city, this.ward, this.municipal, this.district, this.pincode, this.state});
+
+  Address copyWith({ String houseNo, String street, String city, String ward, String municipal, String district, String pincode, String state }) {
+    return Address(
+      houseNo: houseNo ?? this.houseNo,
+      street: street ?? this.street,
+      city: city ?? this.city,
+      ward: ward ?? this.ward,
+      municipal: municipal ?? this.municipal,
+      district: district ?? this.district,
+      pincode: pincode ?? this.pincode,
+      state: state ?? this.state,
+    );
+  }
+
+  @override
+  List<Object> get props => [houseNo, street, city, ward, municipal, district, pincode, state];
+
+//  AddressEntity toEntity() {
+//    return AddressEntity(houseNo: houseNo, street: street, city: city, ward: ward, municipal: municipal, district: district, pincode: pincode, state: state);
+//  }
+//
+//  static Address fromEntity(AddressEntity entity) {
+//    return Address(
+//      houseNo: entity.houseNo,
+//      street: entity.street,
+//      city: entity.city,
+//      ward: entity.ward,
+//      municipal: entity.municipal,
+//      district: entity.district,
+//      pincode: entity.pincode,
+//      state: entity.state,
+//    );
+//  }
+
 }
--- lib/person/model/person.dart
+++ lib/person/model/person.dart
@@ -1,6 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
 import 'address.dart';
 
-class Person {
+@immutable
+class Person extends Equatable{
   final String sno;
   final String name;
   final String mobile;
@@ -16,19 +20,51 @@ class Person {
   final String voterId;
   final List<String> voterUrl;
 
-  Person(
-      this.sno,
-      this.name,
-      this.mobile,
-      this.whatsapp,
-      this.gender,
-      this.dob,
-      this.address,
-      this.occupation,
-      this.aadhaar,
-      this.aadharUrl,
-      this.pancard,
-      this.pancardUrl,
-      this.voterId,
-      this.voterUrl);
+  Person({this.sno, this.name, this.mobile, this.whatsapp, this.gender, this.dob, this.address, this.occupation, this.aadhaar, this.aadharUrl, this.pancard, this.pancardUrl, this.voterId, this.voterUrl});
+
+  Person copyWith({ String sno, String name, String mobile, String whatsapp, String gender, DateTime dob, Address address, String occupation, String aadhaar, List<String> aadharUrl, String pancard, String pancardUrl, String voterId, List<String> voterUrl }) {
+    return Person(
+      sno: sno ?? this.sno,
+      name: name ?? this.name,
+      mobile: mobile ?? this.mobile,
+      whatsapp: whatsapp ?? this.whatsapp,
+      gender: gender ?? this.gender,
+      dob: dob ?? this.dob,
+      address: address ?? this.address,
+      occupation: occupation ?? this.occupation,
+      aadhaar: aadhaar ?? this.aadhaar,
+      aadharUrl: aadharUrl ?? this.aadharUrl,
+      pancard: pancard ?? this.pancard,
+      pancardUrl: pancardUrl ?? this.pancardUrl,
+      voterId: voterId ?? this.voterId,
+      voterUrl: voterUrl ?? this.voterUrl,
+    );
+  }
+
+  @override
+  List<Object> get props => [sno, name, mobile, whatsapp, gender, dob, address, occupation, aadhaar, aadharUrl, pancard, pancardUrl, voterId, voterUrl];
+
+//  PersonEntity toEntity() {
+//    return PersonEntity(sno: sno, name: name, mobile: mobile, whatsapp: whatsapp, gender: gender, dob: dob, address: address, occupation: occupation, aadhaar: aadhaar, aadharUrl: aadharUrl, pancard: pancard, pancardUrl: pancardUrl, voterId: voterId, voterUrl: voterUrl);
+//  }
+
+//  static Person fromEntity(PersonEntity entity) {
+//    return Person(
+//      sno: entity.sno,
+//      name: entity.name,
+//      mobile: entity.mobile,
+//      whatsapp: entity.whatsapp,
+//      gender: entity.gender,
+//      dob: entity.dob,
+//      address: entity.address,
+//      occupation: entity.occupation,
+//      aadhaar: entity.aadhaar,
+//      aadharUrl: entity.aadharUrl,
+//      pancard: entity.pancard,
+//      pancardUrl: entity.pancardUrl,
+//      voterId: entity.voterId,
+//      voterUrl: entity.voterUrl,
+//    );
+//  }
+
 }
